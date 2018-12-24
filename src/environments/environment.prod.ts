63b9cb5fc79410af9b9571f5eb4f4465a2495187
Rémi <remi.1998@hotmail.com>
2018-12-24 18:54:40 +0100

    Small bug fixes 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC7O3Fa6OqzwitYKcZKP3dj-raWJ-tMKmU',
+    authDomain: 'campus-management-4d426.firebaseapp.com',
+    databaseURL: 'https://campus-management-4d426.firebaseio.com',
+    projectId: 'campus-management-4d426',
+    storageBucket: 'campus-management-4d426.appspot.com',
+    messagingSenderId: '466408025671'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebase: {
     apiKey: 'AIzaSyC7O3Fa6OqzwitYKcZKP3dj-raWJ-tMKmU',
     authDomain: 'campus-management-4d426.firebaseapp.com',
